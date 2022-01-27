739e7ec0d6e6c12c9b5d5e977113c65b6597813c
Megan <megan.hagar@gmail.com>
2022-01-27 10:36:44 -0800

    Added some guiding comments and a button for encampment data

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  /// entry point of our whole program so it must always be defined if you want to render something on the screen, can use arrow notation///
+  /// running runApp inside main() function, makes MyApp() root of widget tree (renders widget, and widget's children///
+  /// everything in Flutter is a widget, and each one can have its own set of properties and child widgets ///
+  /// Stateful widget: manages its own internal state and keeps track of it.///
+  /// Stateless widget: while this kind of widget doesn’t manage its own internal state. For example a button.///
+
   runApp(const MyApp());
 }
 
 Map<int, Color> color =
+/// This maps out the color of SHO banner blue for use of the Material Color in the app builder, meaning I can use it for AppBar and buttons///
   {
     50:Color.fromRGBO(134,201,210, .1),
     100:Color.fromRGBO(134,201,210, .2),
@@ -18,25 +25,33 @@ Map<int, Color> color =
     900:Color.fromRGBO(134,201,210, 1),
   };
 
+/// Where I define SHO banner blue as a Material color so I can use it without error ///
 MaterialColor colorCustom = MaterialColor(0xFF86C9D2, color);
 
 class MyApp extends StatelessWidget {
+
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  ///@override marks an instance member as overriding a superclass member with the same name///
   Widget build(BuildContext context) {
+    /// MaterialApp widget defining our app title, our app theme, and our login page. introduces Navigator ///
+    /// is the starting point of your app, it tells Flutter that you are going to use Material components and follow material design in your app///
     return MaterialApp(
+      ///A convenience widget that wraps a number of widgets that are commonly required for material design applications///
       title: 'SHO App',
       theme: ThemeData(
         primarySwatch: colorCustom,
       ),
-      home: const MyHomePage(title: 'Seattle Homeless Outreach App'),
+      home: const MyLoginPage(title: 'Seattle Homeless Outreach App'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class MyLoginPage extends StatefulWidget {
+  /// contain fields that affect how it looks ///
+
+  const MyLoginPage({Key? key, required this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -50,10 +65,12 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyLoginPage> createState() => _MyLoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyLoginPageState extends State<MyLoginPage> {
+  ///  responsible for defining our MyLoginPage widget state ///
+  /// Prefixing an identifier with an underscore enforces privacy in the Dart language and is a recommended best practice for State objects ///
   // int _counter = 0;
 
   // void _incrementCounter() {
@@ -66,10 +83,38 @@ class _MyHomePageState extends State<MyHomePage> {
   //     _counter++;
   //   });
   // }
+
+
+  /// defines the custom font to apply in app UI ///
   TextStyle style = TextStyle(fontFamily: 'Charis SIL Regular', fontSize: 20.0);
 
+  void _goHome() {
+    /// widget that builds the homepage ~ not the login page ///
+    Navigator.of(context).push(
+      ///the Navigator manages a stack containing the app's routes. Pushing a route onto the Navigator's stack updates the display to that route. ///
+      ///Popping a route from the Navigator's stack returns the display to the previous route. ///
+      MaterialPageRoute<void>(
+        builder: (context) {
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Seattle Homless Outreach App'),
+            ),
+            body: const Center(
+              child: Text('Hello World\nDouble'),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
   @override
+  ///override the build function that returns our main widget///
   Widget build(BuildContext context) {
+
+    ///define our UI elements///
+    ///final keyword simply tells our app that the object value won’t be modified throughout the app ///
 
     final emailField = TextField(
       obscureText: false,
@@ -80,8 +125,9 @@ class _MyHomePageState extends State<MyHomePage> {
           border:
               OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
     );
+
     final passwordField = TextField(
-      obscureText: true,
+      obscureText: true,  ///hide the value of the input ///
       style: style,
       decoration: InputDecoration(
           contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
@@ -89,14 +135,24 @@ class _MyHomePageState extends State<MyHomePage> {
           border:
               OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
     );
+
     final loginButon = Material(
-      elevation: 5.0,
+      ///material widget gives us easier access to aesthetic features///
+      elevation: 5.0, ///adds a shadow///
       borderRadius: BorderRadius.circular(30.0),
       color: Color.fromRGBO(190, 32, 46, 1),
       child: MaterialButton(
+        ///child to Material Widget, adds the button, below takes text feature as a child///
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(
+          ///the Navigator manages a stack containing the app's routes. Pushing a route onto the Navigator's stack updates the display to that route. ///
+          ///Popping a route from the Navigator's stack returns the display to the previous route. ///
+            context,
+            MaterialPageRoute(builder: (context) => const NavMainPage()),
+          );
+        }, ///event listener, calls NavMainPage (stateful widget) in the state app///
         child: Text("Login",
             textAlign: TextAlign.center,
             style: style.copyWith(
@@ -105,16 +161,20 @@ class _MyHomePageState extends State<MyHomePage> {
     );
     
     return Scaffold(
+      ///put our widgets together in Scaffold widget ///
+      ///let you implement the material standard app widgets that most application has: AppBar, BottomAppBar, FloatingActionButton, BottomSheet, Drawer, Snackbar///
       body: Center(
         child: Container(
           color: Colors.white,
           child: Padding(
             padding: const EdgeInsets.all(36.0),
             child: Column(
+              /// Column widget allows us to align form elements vertically ///
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 SizedBox(
+                  ///Widget to help with spacing, allos us to define height of image///
                   height: 300.0,
                   child: Image.asset(
                     "assets/images/logotrrans.png",
@@ -137,6 +197,26 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
+    );
+  }
+}
+
+class NavMainPage extends StatelessWidget {
+      /// widget that builds the homepage ~ not the login page ///
+  const NavMainPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Seattle Homless Outreach App'),
+        ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text('Encampment Data'),
+        ),
+      )
     );
   }
 }
