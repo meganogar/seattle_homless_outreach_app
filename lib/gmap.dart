5a5a4c6fb6791bbe2034a4fa842847fd07777a44
Megan <megan.hagar@gmail.com>
2022-02-01 11:56:00 -0800

    Adding the set up for the map, sans API

--- lib/gmap.dart
+++ lib/gmap.dart
@@ -1,6 +1,6 @@
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Gmap extends StatefulWidget {
   const Gmap({ Key? key }) : super(key: key);
@@ -10,15 +10,29 @@ class Gmap extends StatefulWidget {
 }
 
 class _GmapState extends State<Gmap> {
+late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(47.6062, 122.3321);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Map'),
-      ),
-      body: Container(
-        alignment: Alignment.center,
-        child: Text("Coding with Megan"),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('SHO Sample App'),
+          backgroundColor: Color.fromRGBO(134,201,210, .9),
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
       ),
     );
   }
